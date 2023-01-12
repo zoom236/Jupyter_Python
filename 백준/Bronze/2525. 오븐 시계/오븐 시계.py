72e18a9da44e9127931af31ffcbf012b764ddf51
zoom236 <95058400+zoom236@users.noreply.github.com>
2023-01-13 02:22:35 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -2,8 +2,8 @@ H,M = map(int,input().split())
 cook = int(input())
 M2 = cook + M    
 if M2 >= 60 :
-    H += (M2 // 60) 
-    M = (M2) % 60
+    H += M2 // 60 
+    M = M2 % 60
     if H >= 24 :
         H -= 24
 else :
