6cbad73f178f7ab5f306d4821d0586df3cfb5a7c
zoom236 <95058400+zoom236@users.noreply.github.com>
2023-01-27 22:49:31 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,15 +1,10 @@
-word = input().lower() #소문자로 변경
-word_set = list(set(word)) #set 집합함수를 통해, 중복제거 
-com = []
+word = input().lower()
+word_set = list(set(word))
+temp= []
 for i in word_set :
-    count = word.count(i) #처음 입력된 단어에서 중복 된 알파벳 갯수 세기 
-    com.append(count)       
-
-if com.count(max(com)) >= 2 : #max가 같은 수로 2개 이상일때 
-    print('?')
-else:
-    print(word_set[(com.index(max(com)))].upper())
-    #ex) word = baaa
-    #ex) word_set = [b,a]
-    #ex) com = [1,3]
-    #max(com)은 3 index(3)은 com[1]에 위치 word_set[1]은 a를 가리킴
+    count = word.count(i)
+    temp.append(count)
+if temp.count(max(temp)) >= 2 :
+    print("?")
+else :
+    print(word_set[(temp.index(max(temp)))].upper())
